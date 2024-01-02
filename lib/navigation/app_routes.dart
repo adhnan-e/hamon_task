362227e6afd99812672e31eeaa9c7fdb90484ea2
Adhnan <adhnan.e@gmail.com>
2024-01-02 23:38:47 +0530

    codebase setup -completed

--- lib/navigation/app_routes.dart
+++ lib/navigation/app_routes.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hamon_task/navigation/route_constants.dart';
 
+import '../presentation/home/home_screen.dart';
 
 class AppRoutes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final page = switch (settings.name) {
-
+      RouteConstants.home => const HomeScreen(),
       _ => throw Exception('Invalid route')
     };
-
+    debugPrint('<>route ${settings.name}');
+    return MaterialPageRoute(
+      builder: (context) => page,
+    );
   }
 }
